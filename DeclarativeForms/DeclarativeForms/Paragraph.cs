a43f9a63c7288017bfa65002c317d76390607940
Vladimir Aleshechkin <min6220@yandex.ru>
2024-07-17 22:24:03 +0500

    Репозиторий создан

---
+++ DeclarativeForms/DeclarativeForms/Paragraph.cs
@@ -0,0 +1,12 @@
+﻿using ScriptEngine.Machine.Contexts;
+
+namespace osdf
+{
+    [ContextClass("ДфАбзац", "DfParagraph")]
+    public class DfParagraph : DfElement
+    {
+        public DfParagraph(string p1) : base(p1)
+        {
+        }
+    }
+}
